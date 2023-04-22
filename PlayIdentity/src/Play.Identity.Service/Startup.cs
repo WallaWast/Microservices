c8cab59bb696a44ff76b96b8d61519d8d1693732
Wallace Torres <wallacewst@gmail.com>
2023-04-22 11:47:35 -0700

    Authentication

--- PlayIdentity/src/Play.Identity.Service/Startup.cs
+++ PlayIdentity/src/Play.Identity.Service/Startup.cs
@@ -16,6 +16,7 @@ using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using Play.Common.Settings;
 using Play.Identity.Service.Entities;
+using Play.Identity.Service.Settings;
 
 namespace Play.Identity.Service
 {
@@ -34,14 +35,27 @@ namespace Play.Identity.Service
             BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
             var serviceSettings = Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
             var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+            var identityServerSettings = Configuration.GetSection(nameof(IdentityServerSettings)).Get<IdentityServerSettings>();
 
             services.AddDefaultIdentity<ApplicationUser>()
-                .AddRoles<ApplicationRole>()
-                .AddMongoDbStores<ApplicationUser, ApplicationRole, Guid>
-                (
-                    mongoDbSettings.ConnectionString,
-                    serviceSettings.ServiceName
-                );
+                    .AddRoles<ApplicationRole>()
+                    .AddMongoDbStores<ApplicationUser, ApplicationRole, Guid>
+                    (
+                        mongoDbSettings.ConnectionString,
+                        serviceSettings.ServiceName
+                    );
+
+            services.AddIdentityServer(options =>
+                    {
+                        options.Events.RaiseSuccessEvents = true;
+                        options.Events.RaiseFailureEvents = true;
+                        options.Events.RaiseErrorEvents = true;
+                    })
+                    .AddAspNetIdentity<ApplicationUser>()
+                    .AddInMemoryApiScopes(identityServerSettings.ApiScopes)
+                    .AddInMemoryClients(identityServerSettings.Clients)
+                    .AddInMemoryIdentityResources(identityServerSettings.IdentityResources)
+                    .AddDeveloperSigningCredential();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
@@ -65,6 +79,8 @@ namespace Play.Identity.Service
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseIdentityServer();
 
             app.UseAuthorization();
 
