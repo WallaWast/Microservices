6192d3337755edd38fee460795276d31e9a806d8
Wallace Torres <wallacewst@gmail.com>
2023-05-04 19:39:58 -0700

    Front end changes

--- PlayIdentity/src/Play.Identity.Service/Startup.cs
+++ PlayIdentity/src/Play.Identity.Service/Startup.cs
@@ -23,6 +23,8 @@ namespace Play.Identity.Service
 {
     public class Startup
     {
+        private const string AllowedOriginSetting = "AllowedOrigin";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -78,6 +80,13 @@ namespace Play.Identity.Service
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Play.Identity.Service v1"));
+
+                app.UseCors(builder =>
+                {
+                    builder.WithOrigins(Configuration[AllowedOriginSetting])
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                });
             }
 
             app.UseHttpsRedirection();
