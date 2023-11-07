1c32a6b85f734879fc9486e409e63e35c0529d35
Wallace Torres <wallacewst@gmail.com>
2023-11-06 16:53:23 -0800

    Purchase Controller

---
+++ Play.Trading/src/Play.Trading.Service/Controllers/PurchaseController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Play.Trading.Service.Contracts;
+using Play.Trading.Service.Dtos;
+
+namespace Play.Trading.Service.Controllers;
+
+[ApiController]
+[Route("purchase")]
+[Authorize]
+public class PurchaseController : ControllerBase
+{
+    private readonly IPublishEndpoint _publishEndpoint;
+
+    public PurchaseController(IPublishEndpoint publishEndpoint)
+    {
+        _publishEndpoint = publishEndpoint;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostAsync(SubmitPurchaseDto purchase)
+    {
+        var userId = User.FindFirstValue("sub");
+        var correlationId = Guid.NewGuid();
+
+        var message = new PurchaseRequested(
+            Guid.Parse(userId),
+            purchase.ItemId.Value,
+            purchase.Quantity,
+            correlationId
+        );
+
+        await _publishEndpoint.Publish(message);
+
+        return Accepted();
+    }
+}
