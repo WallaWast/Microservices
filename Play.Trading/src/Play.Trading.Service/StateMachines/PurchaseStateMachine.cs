4c91a4ef8ab1025c67d076a671bbf36d9880b56d
Wallace Torres <wallacewst@gmail.com>
2023-11-06 16:41:13 -0800

    Initializing the state machine

--- Play.Trading/src/Play.Trading.Service/StateMachines/PurchaseStateMachine.cs
+++ Play.Trading/src/Play.Trading.Service/StateMachines/PurchaseStateMachine.cs
@@ -1,4 +1,6 @@
+using System;
 using Automatonymous;
+using Play.Trading.Service.Contracts;
 
 namespace Play.Trading.Service.StateMachines;
 
@@ -9,9 +11,33 @@ public class PurchaseStateMachine : MassTransitStateMachine<PurchaseState>
     public State Completed { get; }
     public State Faulted { get; }
 
+    public Event<PurchaseRequested> PurchaseRequested { get; }
 
     public PurchaseStateMachine()
     {
         InstanceState(state => state.CurrentState);
+        ConfigureEvents();
+        ConfigureInitialState();
+    }
+
+    private void ConfigureEvents()
+    {
+        Event(() => PurchaseRequested);
+    }
+
+    private void ConfigureInitialState()
+    {
+        Initially(
+            When(PurchaseRequested)
+                .Then(context =>
+                {
+                    context.Instance.UserId = context.Data.UserId;
+                    context.Instance.ItemId = context.Data.ItemId;
+                    context.Instance.Quantity = context.Data.Quantity;
+                    context.Instance.Received = DateTimeOffset.UtcNow;
+                    context.Instance.LastUpdated = context.Instance.Received;
+                })
+                .TransitionTo(Accepted)
+        );
     }
 }
