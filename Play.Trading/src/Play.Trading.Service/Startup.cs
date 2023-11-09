9ed31538c525a6d54a2a05e70a5399a27bafefb5
Wallace Torres <wallacewst@gmail.com>
2023-11-09 15:43:46 -0800

    Changes to trading

--- Play.Trading/src/Play.Trading.Service/Startup.cs
+++ Play.Trading/src/Play.Trading.Service/Startup.cs
@@ -43,6 +43,8 @@ namespace Play.Trading.Service
 
             services.AddMongo()
                     .AddMongoRepository<CatalogItem>("catalogitems")
+                    .AddMongoRepository<InventoryItem>("inventoryitems")
+                    .AddMongoRepository<ApplicationUser>("users")
                     .AddJwtBearerAuthentication();
 
             AddMassTransit(services);
